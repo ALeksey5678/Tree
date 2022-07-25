dc7f5c893fe3abe280e9d9d27f120f38c0eababe
Aleksey5678 <foritlessonsalex@gmail.com>
2022-07-25 11:02:39 +0300

    Some Project to test git

---
+++ src/GoodDogTest.java
@@ -0,0 +1,13 @@
+public class GoodDogTest {
+    public static void main(String[] args) {
+        GoodDog one=new GoodDog();
+        one.setSize(70);
+        GoodDog two=new GoodDog();
+        two.setSize(8);
+
+        System.out.println("Первый пес "+ one.getSize());
+        System.out.println("Вторая собака" +two.getSize());
+        one.bark();
+        two.bark();
+    }
+}
